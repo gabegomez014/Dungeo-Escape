13cc059b8f6366c6b637524835815601a4d4e073
gabegomez014 <gomezagabriel2@gmail.com>
2021-10-07 15:56:14 -0400

    Enemy AI section complete

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    protected int health;
+    [SerializeField]
+    protected float speed;
+    [SerializeField]
+    protected int gems;
+    [SerializeField]
+    protected Transform pointA, pointB;
+
+    protected Vector3 _currentTarget;
+    protected Animator _anim;
+    protected SpriteRenderer _sprite;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    public virtual void Update()
+    {
+        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Idle"))
+        {
+            // Do nothing if Idle is playing
+            return;
+        }
+
+        Movement();
+    }
+
+    public virtual void Init()
+    {
+        _anim = GetComponentInChildren<Animator>();
+        _sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    public virtual void Movement()
+    {
+        if (_currentTarget == pointA.position)
+        {
+            _sprite.flipX = true;
+        }
+
+        else
+        {
+            _sprite.flipX = false;
+        }
+
+        if (transform.position == pointA.position)
+        {
+            _currentTarget = pointB.position;
+            _anim.SetTrigger("Idle");
+        }
+
+        else if (transform.position == pointB.position)
+        {
+            _currentTarget = pointA.position;
+            _anim.SetTrigger("Idle");
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator _anim;
+    private Animator _swordAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anim = GetComponentInChildren<Animator>();
+        _swordAnim = transform.GetChild(1).GetComponent<Animator>();
+    }
+
+    public void Move(float move)
+    {
+        _anim.SetFloat("Move", Mathf.Abs( move ));
+    }
+
+    public void Jumping()
+    {
+        _anim.SetBool("Jumping", true);
+    }
+
+    public void NotJumping()
+    {
+        _anim.SetBool("Jumping", false);
+    }
+
+    public void RegularAttack()
+    {
+        _anim.SetTrigger("Attack");
+        _swordAnim.SetTrigger("SwordAnimation");
+    }
+}
